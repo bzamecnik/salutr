3546458444ec3d6e89cf4cf123ad95b874f96f61
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-02-11 21:26:53 +0100

    Update the import script.

--- scripts/import-mvcr/download-stats.sh
+++ scripts/import-mvcr/download-stats.sh
@@ -1,3 +1,4 @@
 wget http://www.mvcr.cz/soubor/2013-komplet-zjkr-win-csv.aspx -O given-names-win1250.csv
 wget http://www.mvcr.cz/soubor/2013-komplet-zpkr-win-zip.aspx -O family-names-win1250.csv.zip
 unzip family-names-win1250.csv.zip
+mv zpkr_win.csv family-names-win1250.csv
