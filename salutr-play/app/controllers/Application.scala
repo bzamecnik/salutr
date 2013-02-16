605e9868f1272768abce2c5fa9d62670912703f9
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-02-16 11:46:13 +0100

    Create a simple form and action for declining.

--- salutr-play/app/controllers/Application.scala
+++ salutr-play/app/controllers/Application.scala
@@ -1,12 +1,34 @@
 package controllers
 
-import play.api._
+import play.api.data.Forms._
+import play.api.data._
 import play.api.mvc._
+import play.api._
+import views._
+import com.salutr.declinator.DeclinationService
 
 object Application extends Controller {
-  
+
+  val declineForm = Form(
+    "name" -> nonEmptyText
+  )
+
+  // -- Actions
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(html.index(declineForm, "", ""))
   }
-  
+
+  def declineToVocative = Action { implicit request =>
+    declineForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(html.index(formWithErrors, "", "")),
+      {
+        case (name) => {
+          val vocative = new DeclinationService().declineToVocative(name)
+          Ok(html.index(declineForm, name, vocative))
+        }
+      }
+    )
+  }
+
 }
