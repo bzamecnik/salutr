8862dc6add70ddde65d141653f28ff00d5261b5b
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2013-02-18 22:52:05 +0100

    Decline to vocative using the REST method via jQuery AJAX call.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,37 @@
 $(document).ready(function() {
 	$('#declineForm input[name=name]').focus();
+
+	if (!$("#vocative").text()) {
+		$("#result-container").hide();
+	}
+	$("#declineForm").submit(function() {
+		var name = $("#declineForm input[name=name]").val();
+		if (name) {
+			$.ajax({
+				url : "vocative.json",
+				type : "GET",
+				data : {
+					name : name
+				},
+				success : function(data) {
+					if ($("#result-container").is(":hidden")) {
+						$("#result-container").slideDown("slow", function() {
+							$("#vocative").hide().text(data.vocative).fadeIn();
+						});
+					} else {
+						$("#vocative").fadeOut(function() {
+							$(this).text(data.vocative).fadeIn()
+						});
+					}
+				}
+			});
+		} else {
+			$("#vocative").fadeOut(function() {
+				$("#result-container").slideUp("slow", function() {
+					$(this).hide();
+				});
+			});
+		}
+		return false;
+	});
 });
