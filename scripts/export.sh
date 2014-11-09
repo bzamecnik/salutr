c66e7871d09f5cc82be2ff96637616759844b22c
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-11-09 22:38:06 +0100

    Update the export script.

--- scripts/export.sh
+++ scripts/export.sh
@@ -5,6 +5,6 @@ cut -d , -f 1 single_names.csv |iconv -f UTF-8 -t ASCII//TRANSLIT \
   | sed -e 's/[^a-zA-Z]//g' -e 's/^nominative$/nominativeAscii/' -e 's/^\(.*\)$/"\1"/' > single_names_ascii_name.csv
 paste -d , single_names.csv single_names_ascii_name.csv > single_names_full.csv
 
-#mongoimport -d soykaSpringTest -c name_vocatives --headerline --type csv --file single_names_full.csv
+#mongoimport -d salutr_db -c name_vocatives --headerline --type csv --file single_names_full.csv
 
-zcat single_names_full.csv.gz |mongoimport -d soykacz -c name_vocatives --headerline --type csv
+zcat single_names_full.csv.gz |mongoimport -d salutr_db -c name_vocatives --headerline --type csv
